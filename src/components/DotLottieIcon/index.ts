12026407546733344ba5b61d3dffdba9c394f701
RWB0104 <psj2716@mensakorea.org>
2024-03-07 16:44:35 +0900

    ✨ [FEATURE] 신규 컴포넌트 추가 - dotLottie 아이콘 - 아이콘 카드

---
+++ src/components/DotLottieIcon/index.ts
@@ -0,0 +1,12 @@
+/**
+ * dotLottie 아이콘 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.03.07 Thu 15:34:07
+ */
+
+import DotLottieIcon from './DotLottieIcon';
+
+export default DotLottieIcon;
+
+export * from './DotLottieIcon';
---
+++ src/components/IconCard/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 아이콘 카드 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2024.03.07 Thu 16:24:48
+ */
+
+import IconCard from './IconCard';
+
+export default IconCard;
+
+export * from './IconCard';
